47e15ee80791bd8b34415eedc79dc3bf759d8609
Dave Wilson <parttimehacker@gmail.com>
2021-09-19 10:57:00 -0700

    Update whatami.sh

--- whatami.sh
+++ whatami.sh
@@ -1,3 +1,10 @@
+echo "Raspian Version"
 cat /etc/os-release			
-cat /proc/device-tree/model			
+echo " "
+echo "Device Information"
+cat /proc/device-tree/model		
+echo " "
+echo "Network"
+hostname
+echo " "
 hostname -I			
